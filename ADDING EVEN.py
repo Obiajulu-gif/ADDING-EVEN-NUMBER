2648d3385655214b4069086cb80eb1b496a5b365
Emmanuel Obiajulu Okoye <>
2021-04-08 18:39:53 +0000

    Initial commit

---
+++ ADDING EVEN.py
@@ -0,0 +1,20 @@
+# we intialize it to zero 
+number = 0
+# it line of code will  step up by 2 until it reaches 100
+# it will also add to itself everytime it step up
+for even in range(0, 101, 2):
+    number += even
+print (number)
+
+
+    #OR
+
+
+even_no = 0
+for number in range (1, 101):
+    if number % 2 == 0:
+        even_no += number
+print (even_no)
+
+
+   
